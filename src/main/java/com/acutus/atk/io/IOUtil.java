d8ff42f7a8637c84b0f1915a70dc13273ca2f21b
jaspervdbijl <jaspervdb@spsi.co.za>
2019-10-29 10:42:34 +0200

    Issue 94: Persist Quote Request

--- src/main/java/com/acutus/atk/io/IOUtil.java
+++ src/main/java/com/acutus/atk/io/IOUtil.java
@@ -1,9 +1,8 @@
 package com.acutus.atk.io;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class IOUtil {
 
@@ -15,6 +14,22 @@ public class IOUtil {
             bos.write(buffer, 0, read);
         }
         return bos.toByteArray();
+    }
+
+    public static byte[] readFully(InputStream is, Integer length) throws IOException {
+        try {
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            for (int r = is.read(buffer); r > -1 && (length == null || length == -1 || bos.size() < length); r = is.read(buffer)) {
+                bos.write(buffer, 0, r);
+            }
+            if (length != null && length != -1 && bos.size() != length) {
+                throw new EOFException("Length to read: " + length + " actual: " + bos.size());
+            }
+            return bos.toByteArray();
+        } finally {
+            is.close();
+        }
     }
 
     public static String readAvailableAsStr(InputStream is) throws IOException {
@@ -36,6 +51,35 @@ public class IOUtil {
         byte buffer[] = new byte[1024];
         for (int r = is.read(buffer); r != -1;r = is.read(buffer)) {
             os.write(buffer, 0, r);
+        }
+    }
+
+    public static byte[] readFully(InputStream is) throws IOException {
+        return readFully(is, null);
+    }
+
+    public static byte[] readFully(File file) throws IOException {
+        return readFully(new FileInputStream(file), null);
+    }
+
+
+    public static byte[] zip(InputStream is) throws IOException {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            try (GZIPOutputStream gos = new GZIPOutputStream(bos)) {
+                gos.write(readFully(is));
+                gos.close();
+                return bos.toByteArray();
+            }
+        }
+    }
+
+    public static byte[] zip(String data) throws IOException {
+        return zip(new ByteArrayInputStream(data.getBytes()));
+    }
+
+    public static byte[] unzip(InputStream zip) throws IOException {
+        try (GZIPInputStream fis = new GZIPInputStream(zip)) {
+            return readFully(fis);
         }
     }
 
