29a9d41235be66f407d144d2ff0c7a256ef7801a
jaspervdbijl <jaspervdb@spsi.co.za>
2020-01-30 11:20:17 +0200

    Integrated firebase messaging

--- src/main/java/com/acutus/atk/util/Assert.java
+++ src/main/java/com/acutus/atk/util/Assert.java
@@ -2,7 +2,7 @@ package com.acutus.atk.util;
 
 import com.acutus.atk.util.call.CallNilRet;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.java.Log;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -13,7 +13,7 @@ import java.util.logging.Level;
 /**
  * Created by jaspervdb on 2/24/16.
  */
-@Slf4j
+@Log
 public class Assert {
 
     public static void isTrue(Class<? extends RuntimeException > exClass, boolean value, String msg, Object... params) {
