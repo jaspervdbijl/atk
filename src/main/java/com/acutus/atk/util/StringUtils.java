1289a3d09dcb0b2278e49bf00f4981b1ef80a32a
Jasper van der Bijl <jasper@vanderbijl.co>
2020-05-27 19:38:58 +0200

    Issue 282: Kwebo voucher not displaying on email receipt

--- src/main/java/com/acutus/atk/util/StringUtils.java
+++ src/main/java/com/acutus/atk/util/StringUtils.java
@@ -1,12 +1,16 @@
 package com.acutus.atk.util;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class StringUtils {
 
     public static boolean isEmpty(String value) {
         return value == null || value.trim().isEmpty();
     }
 
-    public static String notEmpty(String value1,String value2) {
+    public static String notEmpty(String value1, String value2) {
         return !isEmpty(value1) ? value1 : value2;
     }
 
@@ -31,5 +35,21 @@ public class StringUtils {
     public static String removeAllASpaces(String string) {
         return removeAll(string, " ");
     }
+
+    public static int indexOf(String string, String value, int position) {
+        Assert.isTrue(position > 0,"Position can't be null");
+        String split[] = string.split(value);
+        return split.length >= position ?
+                IntStream.range(0, position).map(i -> split[i].length()).sum() + value.length() * (position-1)
+                : -1;
+    }
+
+    public static boolean contains(String string, String value, int position) {
+        return indexOf(string, value, position) != -1;
+    }
+    public static String substring(int start, String string, String value, int position) {
+        return string.substring(start,indexOf(string, value, position));
+    }
+
 
 }
