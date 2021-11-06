c85a3e8e4b1f675b765a2374aea6084d04f765fa
Jasper van der Bijl <jasper@vanderbijl.co>
2021-11-06 11:21:25 +0200

    added simpler timer

--- src/main/java/com/acutus/atk/util/Timer.java
+++ src/main/java/com/acutus/atk/util/Timer.java
@@ -1,30 +1,105 @@
 package com.acutus.atk.util;
 
+import com.acutus.atk.util.collection.Two;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-public class Timer {
-    private long start = System.currentTimeMillis();
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public static Timer start() {
-        return new Timer();
+public class Timer extends ArrayList{
+    private List<Time> marks = new ArrayList<>();
+
+    private Timer(String name) {
+        mark(name);
     }
 
-    public TimeHolder elapsed() {
-        return new TimeHolder(System.currentTimeMillis() - start);
+    public static Timer start(String name) {
+        return new Timer(name);
     }
 
-    public TimeHolder processed(int processed) {
-        return new TimeHolder(elapsed().duration / processed);
+    public TimeHolders stop() {
+        return marks.stream().map(a -> a.elapsed()).collect(Collectors
+                .toCollection(TimeHolders::new));
     }
 
+    public Timer mark(String name) {
+        marks.add(new Time(name));
+        return this;
+    }
+
+    public String toString() {
+        return stop().sec();
+    }
+
+    public static interface Timeable {
+        public long getDuration();
+        public String sec();
+        public String milli();
+
+        String processed(int processed);
+    }
+
+    private static class Time {
+        private long start = System.currentTimeMillis();
+        private Long stop;
+        private String name;
+
+        public Time(String name) {
+            this.name = name;
+        }
+
+        public Timeable elapsed() {
+            stop = stop == null ? System.currentTimeMillis() : stop;
+            return new TimeHolder(name, stop.longValue() - start);
+        }
+
+    }
     @AllArgsConstructor
     @Getter
-    public static class TimeHolder {
+    public static class TimeHolder implements Timeable {
+        private String name;
+        @Getter
         private long duration;
 
-        public long sec() {
-            return duration / 1000;
+        public String sec() {
+            return name + " took " + (duration / 1000);
         }
+
+        public String milli() {
+            return name + " took " + (duration);
+        }
+
+        @Override
+        public String processed(int processed) {
+            return name + ": processed " + ((duration/1000) / processed) +" per second";
+        }
+    }
+
+    public static class TimeHolders extends ArrayList<Timeable> implements Timeable {
+
+        @Override
+        public long getDuration() {
+            return stream().mapToLong(a -> a.getDuration()).sum();
+        }
+
+        @Override
+        public String sec() {
+            return stream().map(a -> a.sec()).reduce((s1,s2) -> s1+"\n"+s2).get();
+        }
+
+        @Override
+        public String milli() {
+            return stream().map(a -> a.milli()).reduce((s1,s2) -> s1+"\n"+s2).get();
+        }
+
+
+        @Override
+        public String processed(int processed) {
+            return stream().map(a -> a.processed(processed)).reduce((s1,s2) -> s1+"\n"+s2).get();
+        }
+
     }
 }
