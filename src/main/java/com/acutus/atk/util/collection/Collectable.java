53d41cf3b4c2df0e52ec5767e1dbb7e773642f01
Jasper van der Bijl <jasper@vanderbijl.co>
2023-03-06 21:54:43 +0200

    Add batch update capability

--- src/main/java/com/acutus/atk/util/collection/Collectable.java
+++ src/main/java/com/acutus/atk/util/collection/Collectable.java
@@ -1,17 +1,42 @@
 package com.acutus.atk.util.collection;
 
 import com.acutus.atk.reflection.Reflect;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.acutus.atk.util.AtkUtil.handle;
 
-public abstract class Collectable {
+@Slf4j
+public abstract class Collectable implements Closeable, AutoCloseable {
 
     public abstract Collectable initFromList(List values);
 
     public List getValues() {
         return Reflect.getFields(getClass()).stream().map(field -> handle(() -> field.get(this))).collect(Collectors.toList());
     }
+
+    @Override
+    public void close() throws IOException {
+        for (Object o : getValues()) {
+            if (o instanceof Closeable) {
+                try {
+                    ((Closeable) o).close();
+                } catch (Exception ex) {
+                    log.warn(ex.getMessage(),ex);
+                }
+            }
+            if (o instanceof AutoCloseable) {
+                try {
+                    ((AutoCloseable) o).close();
+                } catch (Exception ex) {
+                    log.warn(ex.getMessage(),ex);
+                }
+            }
+        }
+    }
+
 }
