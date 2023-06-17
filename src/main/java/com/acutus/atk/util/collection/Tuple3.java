5b190aa1493170e5f8465c1376a2746abd4d3dc1
jaspervdbijl <jaspervdb@cellstop.org>
2023-06-17 15:37:34 +0200

    https://cellstop.atlassian.net/browse/VD-322

--- src/main/java/com/acutus/atk/util/collection/Tuple3.java
+++ src/main/java/com/acutus/atk/util/collection/Tuple3.java
@@ -26,6 +26,21 @@ public class Tuple3<A, B, C> extends Collectable {
         return c;
     }
 
+    public Tuple3<A, B, C> setFirst(A a) {
+        this.a = a;
+        return this;
+    }
+
+    public Tuple3<A, B, C> setSecond(B b) {
+        this.b = b;
+        return this;
+    }
+
+    public Tuple3<A, B, C> setThird(C c) {
+        this.c = c;
+        return this;
+    }
+
     @Override
     public Collectable initFromList(List values) {
         Assert.isTrue(values.size() > 1, "Values less then 3");
